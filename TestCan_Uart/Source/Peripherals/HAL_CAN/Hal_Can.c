584e8c16dfad8f3d8b14ea4c9322e5c367741744
burmistrok <ok.burmitr@gmail.com>
2023-11-14 18:16:25 +0200

    Solved problem with RxPin(configuration was wrong)

--- TestCan_Uart/Source/Peripherals/HAL_CAN/Hal_Can.c
+++ TestCan_Uart/Source/Peripherals/HAL_CAN/Hal_Can.c
@@ -13,53 +13,34 @@
 #define FOUR_BYTES_DATA_SIZE	(32u)
 #define HALF_DATA				(MAX_STD_DLC_LENGHT/2u)
 
+
+
 static uint8_t u8_CanStatus;
 static volatile uint8_t RxIndx;
+
+
+inline static void Can_EnableClock(void);
+inline static void Can_ConfigPins(void);
+inline static void Can_GeneralConfig(void);
+inline static void Can_ConfigBaudRate(void);
+inline static void Can_ConfigFilters(void);
 
 void vCan_Init(void)
 {
 	u8_CanStatus = 0u;
-	RCC->APB1ENR|= RCC_APB1ENR_CAN1EN; 		/*Enable Can Clock*/
-	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);	/*Enable PortA Clock*/
 
-	/*Set RX and Tx Pin*/
-#ifndef USECMSIS_FORCAN
-	/* PA11 - CAN_RX */
-	GPIOA->CRH	&= ~GPIO_CRH_CNF11;   /* CNF11 = 00 */
-	GPIOA->CRH	|= GPIO_CRH_CNF11_1;  /* CNF11 = 10 -> AF Out | Push-pull (CAN_RX) */
-	GPIOA->CRH 	|= GPIO_CRH_MODE11;   /* MODE8 = 11 -> Maximum output speed 50 MHz */
-	/* PA12 - CAN_TX */
-	GPIOA->CRH	&= ~GPIO_CRH_CNF12;	  /* CNF12 = 00 */
-	GPIOA->CRH	|= GPIO_CRH_CNF12_1;	/* CNF12 = 10 -> AF Out | Push-pull (CAN_TX) */
-	GPIOA->CRH 	|= GPIO_CRH_MODE12;   /* MODE8 = 11 -> Maximum output speed 50 MHz */
-#else
-	LL_GPIO_AF_RemapPartial1_CAN1();				/*Remap A11 and A12 to can RX and TX*/
-	/*RxPin*/
-	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_11, LL_GPIO_MODE_FLOATING);
-	LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_11, LL_GPIO_SPEED_FREQ_HIGH);
-	/*TxPin*/
-	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_12, LL_GPIO_MODE_ALTERNATE);
-	LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_12, LL_GPIO_SPEED_FREQ_HIGH);
-#endif
+	Can_EnableClock();
+	Can_ConfigPins();
+
 	CAN1->MCR |= CAN_MCR_INRQ;						/*Start initialization*/
 	while( 0u == (CAN1->MSR & CAN_MSR_INAK_Msk)); 	/*wait Entery in Init state*/
-	CAN1->MCR &= ~CAN_MCR_DBF;						/*0: CAN working during debug*/
-	CAN1->MCR |= CAN_MCR_TXFP; 						/* Priority driven by the request order (chronologically)*/
-	CAN1->MCR |= CAN_MCR_AWUM;						/*Wake up at message on buss*/
-
-	/*Config baud rate to 250k*/
-	CAN1->BTR &= ~CAN_BTR_BRP;
-	CAN1->BTR |= 8U << CAN_BTR_BRP_Pos; /* 36MHz/8 =4.5MHz */
-	CAN1->BTR &= ~(0xFU << CAN_BTR_TS1_Pos);
-	CAN1->BTR |= 12U << CAN_BTR_TS1_Pos;
-	CAN1->BTR &= ~(7U << CAN_BTR_TS2_Pos);
-	CAN1->BTR |=   1U << CAN_BTR_TS2_Pos;
 
 
-#ifndef CAN_DBG_USED
-	//CAN1->BTR &=~CAN_BTR_SILM;
-	//CAN1->BTR |= CAN_BTR_LBKM;
-#endif
+	Can_GeneralConfig();
+	Can_ConfigBaudRate();
+	Can_ConfigFilters();
+
+
 	CAN1->MCR &= ~CAN_MCR_INRQ_Msk; /*Enter Normal Mode*/
 	//while( 0u != ((CAN1->MSR & CAN_MSR_INAK_Msk) | (CAN1->MSR &CAN_MCR_SLEEP_Msk)) );
 
@@ -70,6 +51,7 @@ void Hal_Can_MainFunction(void)
 	TS_CanFrame ls_CanData = {0u};
 	if(0u == u8_CanStatus)
 	{
+
 		ls_CanData.u32CanFrameId = 0x50;
 		ls_CanData.u8_DLC = 8u;
 		ls_CanData.ua8_Data[0u] = 0xAA;
@@ -78,7 +60,7 @@ void Hal_Can_MainFunction(void)
 		ls_CanData.ua8_Data[6u] = 0xAA;
 		ls_CanData.ua8_Data[7u] = 0xAA;
 
-		//Hal_Can_Transmit(&ls_CanData);
+		Hal_Can_Transmit(&ls_CanData);
 		u8_CanStatus = 1u;
 	}
 
@@ -137,4 +119,59 @@ void Hal_Can_Transmit( TS_CanFrame *lptr_FrameInfo)
 
 	CAN1->sTxMailBox[0].TIR |= CAN_TI0R_TXRQ;	/*Requeste transmition*/
 
+}
+
+
+inline static void Can_EnableClock(void)
+{
+	RCC->APB1ENR|= RCC_APB1ENR_CAN1EN; 		/*Enable Can Clock*/
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);	/*Enable PortA Clock*/
+}
+inline static void Can_ConfigPins(void)
+{
+	LL_GPIO_AF_RemapPartial2_CAN1();				/*Remap A11 and A12 to can RX and TX*/
+	/*RxPin*/
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_8, LL_GPIO_MODE_FLOATING);
+	/*TxPin*/
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_9, LL_GPIO_MODE_ALTERNATE);
+	LL_GPIO_SetPinSpeed(GPIOB, LL_GPIO_PIN_9, LL_GPIO_SPEED_FREQ_HIGH);
+}
+inline static void Can_GeneralConfig(void)
+{
+	CAN1->MCR &= ~CAN_MCR_DBF;						/*0: CAN working during debug*/
+	CAN1->MCR |= CAN_MCR_TXFP; 						/* Priority driven by the request order (chronologically)*/
+	CAN1->MCR |= CAN_MCR_AWUM;						/*Wake up at message on buss*/
+}
+inline static void Can_ConfigBaudRate(void)
+{
+	/*Config baud rate to 500k*/
+	CAN1->BTR &= ~CAN_BTR_BRP;
+	CAN1->BTR |= 4U << CAN_BTR_BRP_Pos; /* 36MHz/4 =9MHz */
+	CAN1->BTR &= ~(CAN_BTR_TS1_Msk);
+	CAN1->BTR |= CAN_BTR_TS1_0 | CAN_BTR_TS1_1 | CAN_BTR_TS1_2 | CAN_BTR_TS1_3;		/* Seg1 = 15 */
+	CAN1->BTR &= ~(CAN_BTR_TS2_Msk);
+	CAN1->BTR |= CAN_BTR_TS2_1;			/* Seg2 = 2 */
+
+
+#ifdef CAN_DBG_USED
+	CAN1->BTR &=~CAN_BTR_SILM;
+	CAN1->BTR |= CAN_BTR_LBKM;
+#endif
+}
+inline static void Can_ConfigFilters(void)
+{
+	/*Receive all the messagess*/
+	CAN1->sFilterRegister[0].FR1 = 0xFFFFFFFF;
+	CAN1->sFilterRegister[0].FR2 = 0xFFFFFFFF;
+
+	/* Enable first filter */
+	CAN1->FA1R |= CAN_FA1R_FACT0;
+
+	//CAN1->FFA1R &= ~(0x01);
+
+	CAN1->FS1R |= CAN_FS1R_FSC0;	/* Single 32-bit scale configuration for filter 1*/
+
+	//CAN1->FM1R &= ~(0x01); /*Mask mode*/
+
+	CAN1->FMR &= ~CAN_FMR_FINIT;	/* Activate Filters */
 }
